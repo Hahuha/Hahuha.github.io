fc1c40b58e458d2ee94bee6b82781e92274f35dd
carvallegro <valentin.carmignac@gmail.com>
2016-04-16 00:06:31 +0200

    Added first version of keyboard control
    Basic implementation of an keyboard arrow controller.

--- js/blog_manager.js
+++ js/blog_manager.js
@@ -1,67 +1,89 @@
-var BlogManager= (function() {
+var BlogManager = (function() {
   var pages = {
       home: 0,
       list: 1,
       content: 2
     },
-    data =  {
-      articles : null,
-      projects : null
+    data = {
+      articles: null,
+      projects: null
     },
-    loadError = {
-      hasError: false,
-      messages: []
-    };
+    keyBinding = {
+      37: "left",
+      38: "up",
+      39: "right"
+    }
+  loadError = {
+    hasError: false,
+    messages: []
+  };
 
   function init() {
     resetError();
     $('.go-up').click(function() {
       var index = $(this).data('index');
-      toPage (transitions.up, index - 1 );
+      toPage(transitions.up, index - 1);
     });
 
-    $('.breadcrumb [data-level="0"]').click(function () {
-      toPage (transitions.up, pages.home);
-    });
-    $('.breadcrumb [data-level="1"]').click(function () {
-      toPage (transitions.up, pages.list);
+    //pt-page-current
+    // On bind les flèches du clavier avec les flèches du menu
+    $(document).keydown(function(e) {
+      var direction = keyBinding[e.which];
+      if (!direction) {
+        return;
+      }
+
+      var button = $('.pt-page-current .go-' + direction);
+      if ($(button).length) {
+        $(button).click();
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+      }
     });
 
-    $('#cat-link .articles').click(function () {
+    $('.breadcrumb [data-level="0"]').click(function() {
+      toPage(transitions.up, pages.home);
+    });
+    $('.breadcrumb [data-level="1"]').click(function() {
+      toPage(transitions.up, pages.list);
+    });
+
+    $('#cat-link .articles').click(function() {
       $('#list .wrapper, #content .wrapper').removeClass('projects').addClass('articles');
       $('#list .wrapper h1').text('Articles');
       $('#list .list').empty();
       $('.breadcrumb [data-level="1"]').text("articles");
-      _.each(data.articles, function (value, key, list) {
-        createListElement ('articles', value);
+      _.each(data.articles, function(value, key, list) {
+        createListElement('articles', value);
       });
-      toPage (transitions.down, pages.list);
+      toPage(transitions.down, pages.list);
     });
 
-    $('#cat-link .projects').click(function () {
+    $('#cat-link .projects').click(function() {
       $('#list .wrapper, #content .wrapper').removeClass('articles').addClass('projects');
       $('#list .wrapper h1').text('Projects');
       $('#list .list').empty();
       $('.breadcrumb [data-level="1"]').text("projects");
-      _.each(data.projects, function (value, key, list) {
-        createListElement ('projects', value);
+      _.each(data.projects, function(value, key, list) {
+        createListElement('projects', value);
       });
-      toPage (transitions.down, pages.list);
+      toPage(transitions.down, pages.list);
     });
   }
 
-  function createListElement (section, value) {
+  function createListElement(section, value) {
     $('#content .breadcrumb li:last-child').text(value.title);
-    var listElem = '<li><a href="#" data-id="'+value.id+'"><span class="date">'+value.date+'</span><span class="title">'+value.title+'</span></a></li>';
-    $(listElem).appendTo('#list .list').children('a').click(function () {
-      var result = _.findWhere(data[section], {id: $(this).data("id")});
+    var listElem = '<li><a href="#" data-id="' + value.id + '"><span class="date">' + value.date + '</span><span class="title">' + value.title + '</span></a></li>';
+    $(listElem).appendTo('#list .list').children('a').click(function() {
+      var result = _.findWhere(data[section], {
+        id: $(this).data("id")
+      });
       if (result != undefined) {
         $('#content h1').text(result.title);
         $('#content .article').html(decodeURI(result.content));
         console.log(result.content);
         console.log(decodeURI(result.content));
       }
-      toPage (transitions.down, pages.content);
+      toPage(transitions.down, pages.content);
     });
   }
 
