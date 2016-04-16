f456da52438bf6e7a7b2ebeb9aa74a376860a160
carvallegro <valentin.carmignac@gmail.com>
2016-04-16 14:07:25 +0200

    Improved navigation

--- js/blog_manager.js
+++ js/blog_manager.js
@@ -12,32 +12,40 @@ var BlogManager = (function() {
       37: "left",
       38: "up",
       39: "right"
-    }
-  loadError = {
-    hasError: false,
-    messages: []
-  };
+    },
+    loadError = {
+      hasError: false,
+      messages: []
+    };
 
   function init() {
     resetError();
-    $('.go-up').click(function() {
-      var index = $(this).data('index');
-      toPage(transitions.up, index - 1);
+    $('.menu [class^="go-"]:not(.hidden)').click(function() {
+      if ($(this).hasClass('go-up')) {
+        var index = $(this).data('index');
+        toPage(transitions.up, index - 1);
+        return;
+      }
+      var parent = $(this).parents('.pt-page');
+      var newIndex = 0;
+      if ($(parent).attr('id') === "content") {
+        newIndex = $(parent).data('post-index');
+        newIndex += $(this).hasClass('go-right') ? +1 : -1;
+        loadPost(newIndex);
+      }
     });
 
-    //pt-page-current
     // On bind les flèches du clavier avec les flèches du menu
     $(document).keydown(function(e) {
       var direction = keyBinding[e.which];
       if (!direction) {
         return;
       }
-
-      var button = $('.pt-page-current .go-' + direction);
+      var button = $('.pt-page-current .go-' + direction + ':not(.hidden)');
       if ($(button).length) {
         $(button).click();
-        e.preventDefault(); // prevent the default action (scroll / move caret)
       }
+      e.preventDefault(); // prevent the default action (scroll / move caret)
     });
 
     $('.breadcrumb [data-level="0"]').click(function() {
@@ -47,41 +55,47 @@ var BlogManager = (function() {
       toPage(transitions.up, pages.list);
     });
 
-    $('#cat-link .articles').click(function() {
-      $('#list .wrapper, #content .wrapper').removeClass('projects').addClass('articles');
-      $('#list .wrapper h1').text('Articles');
+    $('#cat-link a').click(function() {
+      var newSection = $(this).data('section');
+      var oldSection = newSection == 'articles' ? 'projects' : 'articles';
+      $('#list .wrapper, #content .wrapper').removeClass(oldSection).addClass(newSection);
+      $('#list .wrapper h1').text(newSection);
       $('#list .list').empty();
-      $('.breadcrumb [data-level="1"]').text("articles");
-      _.each(data.articles, function(value, key, list) {
-        createListElement('articles', value);
+      $('#list').data('section', newSection);
+      $('.breadcrumb [data-level="1"]').text(newSection);
+      _.each(data.articles, function(value, index, list) {
+        createListElement(value, index);
       });
       toPage(transitions.down, pages.list);
     });
 
-    $('#cat-link .projects').click(function() {
-      $('#list .wrapper, #content .wrapper').removeClass('articles').addClass('projects');
-      $('#list .wrapper h1').text('Projects');
-      $('#list .list').empty();
-      $('.breadcrumb [data-level="1"]').text("projects");
-      _.each(data.projects, function(value, key, list) {
-        createListElement('projects', value);
-      });
-      toPage(transitions.down, pages.list);
-    });
   }
 
-  function createListElement(section, value) {
+  function createListElement(value, index) {
+    var section = $('#list').data('section');
     $('#content .breadcrumb li:last-child').text(value.title);
+
     var listElem = '<li><a href="#" data-id="' + value.id + '"><span class="date">' + value.date + '</span><span class="title">' + value.title + '</span></a></li>';
     $(listElem).appendTo('#list .list').children('a').click(function() {
       var result = _.findWhere(data[section], {
         id: $(this).data("id")
       });
+
+      // Manage index of post in data and navigation
+      $('#content [class^="go-"]').removeClass('hidden');
+      if (index == 0) {
+        $('#content .go-left').addClass('hidden');
+      }
+      else if (index >= data[section].length-1) {
+        $('#content .go-right').addClass('hidden');
+      }
+
+      // Manage post content
       if (result != undefined) {
+        $('#content').data('post-index', index);
+        $('#content').data('post-id', result.id);
         $('#content h1').text(result.title);
         $('#content .article').html(decodeURI(result.content));
-        console.log(result.content);
-        console.log(decodeURI(result.content));
       }
       toPage(transitions.down, pages.content);
     });
@@ -90,9 +104,7 @@ var BlogManager = (function() {
   function load() {
     var articleLoading = $.getJSON('/feed_article.json')
       .done(function(obj) {
-        console.log("article");
         data.articles = obj;
-        console.log(data.articles);
       })
       .fail(function(jqxhr, textStatus, error) {
         loadError.hasError = true;
@@ -104,9 +116,7 @@ var BlogManager = (function() {
 
     $.getJSON('/feed_project.json')
       .done(function(obj) {
-        console.log("project");
         data.projects = obj;
-        console.log(data.projects);
       })
       .fail(function(jqxhr, textStatus, error) {
         loadError.hasError = true;
@@ -133,19 +143,33 @@ var BlogManager = (function() {
     });
   }
 
-  function list(options) {
-
-  }
-
-  function content(options) {
-    // body...
-  }
-
   function resetError() {
     loadError = {
       hasError: false,
       messages: []
     };
+  }
+
+  function loadPost(postIndex) {
+    var section = $('#list').data('section');
+    var post = data[section][postIndex];
+
+    // Manage index of post in data and navigation
+    $('#content [class^="go-"]').removeClass('hidden');
+    if (postIndex == 0) {
+      $('#content .go-left').addClass('hidden');
+    }
+    else if (postIndex >= data[section].length-1) {
+      $('#content .go-right').addClass('hidden');
+    }
+
+    // Manage post content
+    if (post != undefined) {
+      $('#content').data('post-index', postIndex);
+      $('#content').data('post-id', post.id);
+      $('#content h1').text(post.title);
+      $('#content .article').html(decodeURI(post.content));
+    }
   }
 
   init();
