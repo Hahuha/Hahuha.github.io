52c426c53da2a77d872dd1ed64a115bd236518f9
carvallegro <valentin.carmignac@gmail.com>
2016-05-18 22:34:38 +0200

    Refactored JS and file structure
    Separated access to blog data and blog navigation.
    created a dedicated layout so each part of the blog calls it with customed parameters

--- js/main.js
+++ js/main.js
@@ -1,20 +1,6 @@
-/*
- * Page transitions
- */
 $(function() {
 
-  var bManager = BlogManager;
-  BlogManager.load();
-  // while (bManager.loadError.hasError === false || (bManager.articles === null && bManager.projects === null)) {
-  //   console.log("loaded !");
-  // }
-  // bManager.resetError();
+  // First init
+  jmanager.init(initData);
 
-  // Gestion de l'ouverture de la section "About Me"
-  $('#about .close-btn').click(function () {
-    $('#about').addClass('closed').removeClass('opened');
-  });
-  $('.menu .info').click(function () {
-    $('#about').addClass('opened').removeClass('removed');
-  })
 });
